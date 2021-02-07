71a1816126c39999a6a0b58ca6cdc6ce92e012f4
padosum <makalu171@gmail.com>
2021-02-07 15:54:34 +0900

    🔥 remove unused file

--- static/createlink.js
+++
@@ -1,5 +0,0 @@
-(function() {
-    let content = document.querySelector('.post-html');
-    content.innerHTML = content.innerHTML.replace(/\[\[(.+?)\]\]\{(.+?)\}/g, '<a href="../$1">$2</a>');
-    content.innerHTML = content.innerHTML.replace(/\[\[(.+?)\]\]/g, '<a href="../$1">$1</a>');
-})();
