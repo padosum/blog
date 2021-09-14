047c99f4546f8525af4f915fbd4ae8430f408d5d
padosum <makalu171@gmail.com>
2021-09-14 23:02:53 +0900

    🔥 Remove files
    - change theme caused unused files
    - rename files

--- src/gatsby-theme-garden/components/mdx-components/index.js
+++
@@ -1,5 +0,0 @@
-// the components provided by the theme
-export { AnchorTag as a } from "./anchor-tag";
-
-// your own component to inject into mdx
-export code from "./CodeBlock"; // any code block will use this component
