ec4af5459eb9727276c9b24b930b7b524e09cfec
padosum <makalu171@gmail.com>
2021-10-25 18:42:48 +0900

    💄 update custom theme color
    - change placeholder text color
    - change header color

---
+++ .layouts/src/gatsby-theme-primer-wiki/theme.js
@@ -0,0 +1,28 @@
+const theme = {
+  colorSchemes: {
+    light: {
+      colors: {
+        header: {
+          bg: "#1a8dee",
+          text: "#ffffff"
+        },
+        auto: {
+          gray: [
+            "#fafbfc",
+            "#f6f8fa",
+            "#e1e4e8",
+            "#d1d5da",
+            "#959da5",
+            "#6a737d",
+            "#586069",
+            "#e1e4e8",
+            "#2f363d",
+            "#24292e"
+          ],
+        }
+      },
+    },
+  },
+};
+
+export default theme;
