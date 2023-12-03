c5671a9242c8773ad9e92f7f2dd93a1413a66864
Yeonjeong Choi <padosum.yj@gmail.com>
2023-12-03 10:59:55 +0900

    🎨 theme color 수정

--- .layouts/src/gatsby-theme-primer-wiki/theme.js
+++ .layouts/src/gatsby-theme-primer-wiki/theme.js
@@ -7,16 +7,21 @@ const theme = {
     light: {
       colors: {
         auto: {
-          white: '#ede0de',
+          white: '#fbfdfc',
+          gray: ['#0ac266', '#1f5c7a', '#1f5c7a'],
         },
         header: {
-          bg: '#0ac266',
+          bg: '#fbfdfc',
           text: '#1f5c7a',
+          border: '#0ac266',
         },
         bg: {
           primary: '#fbfdfc',
           alt: 'rgba(255, 255, 255, 0.07)',
           title: '#00007f',
+        },
+        headerSearch: {
+          bg: 'hsl(99, 59%, 70%, 0.5)',
         },
         text: {
           primary: '#2c293d',
@@ -24,6 +29,7 @@ const theme = {
           placeholder: 'hsl(251, 13%, 31%)',
           neutralMuted: 'rgba(175,184,193,0.2)',
           alt: '#fbfdfc',
+          secondary: '#1f5c7a',
         },
         border: {
           primary: 'hsl(251, 13%, 31%)',
@@ -33,15 +39,28 @@ const theme = {
     },
     dark: {
       colors: {
+        auto: {
+          white: '#000000',
+          gray: ['#0ac266'],
+        },
+        header: {
+          text: '#a5e085',
+          border: '#0ac266',
+        },
         bg: {
-          primary: 'hsl(27, 14%, 15%)',
+          primary: '#121212',
+        },
+        headerSearch: {
+          bg: 'hsl(99, 59%, 70%, 0.5)',
         },
         text: {
-          primary: '#ffffff',
+          primary: '#fbfdfc',
+          secondary: '#a5e085',
+          placeholder: '#ede0de',
           neutralMuted: 'rgba(110,118,129,0.4)',
+          alt: '#fbfdfc',
         },
-
-        gray: ['hsl(27, 15%, 12%)'],
+        gray: ['#0ac266'],
       },
     },
   },
